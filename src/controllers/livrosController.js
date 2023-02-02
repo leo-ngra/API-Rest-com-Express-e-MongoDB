04344fd60062f867975bc9c5ad5dbc313a30d028
leo-ngra <leonardo.ngra@gmail.com>
2023-02-02 10:08:52 -0300

    Inserindo busca livros por ID

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,19 @@ class LivroController {
     })
     }
 
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros) => {
+            
+            if(err) {
+                res.status(400).send({message: `${err.message} - ID do livro não atualizado.`});
+            } else {
+                res.status(200).send(livros);
+            }
+        })
+    }
+
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
 
